1e788f19eb0d2ae2c7c58d2d38603593e7010526
Brandon Mtz <bmtz5656@gmail.com>
2023-03-23 09:53:19 -0600

    Interfaz, Binarización, Compo, Suma y Resta
    *Se cambió la interfaz para usar imágenes de 512px
    *Se añadieron los filtros de binarización, Composición, Suma y Resta
    *Se avanzó en la parte de uso de la segunda imagen para añadirle más filtros (WIP)

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
-    static String PATH = "C:\\Users\\daros\\OneDrive\\Escritorio\\Fotos\\bob.png";
+
+    //static String PATH = "C:\\Users\\daros\\OneDrive\\Escritorio\\Fotos\\bob.png";
+    static String PATH = "C:\\Users\\bmtz5\\OneDrive\\Imágenes\\imagen.jpg";
     public static void main(String[] args) {
         System.out.println("Ejecutando proyecto");
 
--- src/Ventana.java
+++ src/Ventana.java
@@ -6,14 +6,20 @@ import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.awt.image.BufferedImage;
+import java.awt.image.RenderedImage;
 import java.io.File;
 import java.io.IOException;
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+import java.awt.Robot;
+import java.awt.Toolkit;
 
 public class Ventana extends JFrame implements ActionListener {
     private JPanel contentPanel;
     private ImageIcon imagenOriginal, imagenProcesada;
     private JLabel contenedorImgOriginal, contenedorImgProcesada, histograma1, histograma2;
-    private JButton btnNegativo, btnGris, btnBrillo;
+    private JButton btnNegativo, btnGris, btnBrillo, btnBinarizacion, btnUmbral, btnComposicion, btnSuma, btnResta;
 
     double[][] back;
     double n = 0;
@@ -44,7 +50,7 @@ public class Ventana extends JFrame implements ActionListener {
         setJMenuBar(barraMenu);
 
         //Asignamos la posicion inicial y las dimensiones de la ventana.
-        setBounds(250, 100, 700, 600);
+        setBounds(50, 50, 1400, 900);
 
         //Creamos el contenedor dentro del JFrame para despues agregar todos los elementos que tendra la interfaz
         contentPanel = new JPanel();
@@ -55,20 +61,20 @@ public class Ventana extends JFrame implements ActionListener {
         imagenOriginal = new ImageIcon(PATH);
 //        imagenProcesada = new ImageIcon("C:\\Users\\daros\\OneDrive\\Escritorio\\Fotos\\bob.png");
 
-
         //IMAGEN ORIGINAL
         contenedorImgOriginal = new JLabel(imagenOriginal);
-        contenedorImgOriginal.setBounds(100, 100, imagenOriginal.getIconWidth(), imagenOriginal.getIconHeight());
+        contenedorImgOriginal.setBounds(50, 50, imagenOriginal.getIconWidth(), imagenOriginal.getIconHeight());
+
         //HISTOGRAMA
         histograma1 = new JLabel();
         histograma1.setBorder(javax.swing.BorderFactory.createLineBorder(new Color(0, 0, 0)));
-        histograma1.setBounds(100, 400, 200, 50);
+        histograma1.setBounds(50, 600, imagenOriginal.getIconWidth(), 100);
         contentPanel.add(histograma1);
 
 
         //IMAGEN PROCESADA
         contenedorImgProcesada = new JLabel();
-        contenedorImgProcesada.setBounds(400, 100, imagenOriginal.getIconWidth(), imagenOriginal.getIconHeight());
+        contenedorImgProcesada.setBounds(650, 50, imagenOriginal.getIconWidth(), imagenOriginal.getIconHeight());
 
         contentPanel.add(contenedorImgOriginal);
         contentPanel.add(contenedorImgProcesada);
@@ -76,24 +82,48 @@ public class Ventana extends JFrame implements ActionListener {
 
         //BOTONES
         btnNegativo = new JButton("Negativo");
-        btnNegativo.setBounds(100, 500, 100, 20);
+        btnNegativo.setBounds(50, 750, 100, 20);
         btnNegativo.addActionListener(this);
         contentPanel.add(btnNegativo);
 
         btnGris = new JButton("Gris");
-        btnGris.setBounds(220, 500, 100, 20);
+        btnGris.setBounds(170, 750, 100, 20);
         btnGris.addActionListener(this);
         contentPanel.add(btnGris);
 
         btnBrillo = new JButton("Brillo");
-        btnBrillo.setBounds(340, 500, 100, 20);
+        btnBrillo.setBounds(290, 750, 100, 20);
         btnBrillo.addActionListener(this);
         contentPanel.add(btnBrillo);
 
+        btnBinarizacion = new JButton("Binarizacion");
+        btnBinarizacion.setBounds(410, 750, 100, 20);
+        btnBinarizacion.addActionListener(this);
+        contentPanel.add(btnBinarizacion);
 
+        btnUmbral = new JButton("Umbral");
+        btnUmbral.setBounds(530, 750, 100, 20);
+        btnUmbral.addActionListener(this);
+        contentPanel.add(btnUmbral);
+
+        btnComposicion = new JButton("Composicion");
+        btnComposicion.setBounds(650, 750, 100, 20);
+        btnComposicion.addActionListener(this);
+        contentPanel.add(btnComposicion);
+
+        btnSuma = new JButton("Suma");
+        btnSuma.setBounds(770, 750, 100, 20);
+        btnSuma.addActionListener(this);
+        contentPanel.add(btnSuma);
+
+        btnResta = new JButton("Resta");
+        btnResta.setBounds(900, 750, 100, 20);
+        btnResta.addActionListener(this);
+        contentPanel.add(btnResta);
 
         setVisible(true);
     }
+
 
     public void actionPerformed(ActionEvent e) {
         System.out.println(e.paramString());
@@ -137,6 +167,7 @@ public class Ventana extends JFrame implements ActionListener {
 
             ImageIcon prueba = new ImageIcon(imgN);
 
+
             contenedorImgProcesada.setIcon(prueba);
         } else if (e.paramString().indexOf("Gris") != -1) {
             System.out.println("Gris");
@@ -159,7 +190,7 @@ public class Ventana extends JFrame implements ActionListener {
             }
 
             ImageIcon prueba = new ImageIcon(imgN);
-
+            contenedorImgOriginal.setIcon(imagenOriginal);
             contenedorImgProcesada.setIcon(prueba);
         } else if(e.paramString().indexOf("Brillo") != -1){
             System.out.println("Brillo");
@@ -216,6 +247,226 @@ public class Ventana extends JFrame implements ActionListener {
             ImageIcon prueba = new ImageIcon(finalImgB);
 
             contenedorImgProcesada.setIcon(prueba);
+
+        //Se obtienen los valores de RGB, se hace una suma ponderada de estos valores y si es mayor a 128 el pixel se establece en blanco, sino se establece en negro
+        }else if(e.paramString().indexOf("Binarizacion") != -1){
+            System.out.println("Binarización");
+
+            BufferedImage imgBin = null;
+            try {
+                imgBin = ImageIO.read(new File(PATH));
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+
+            BufferedImage bin = new BufferedImage(ancho, alto, BufferedImage.TYPE_BYTE_BINARY);
+
+            for (int i = 0; i < ancho; i++) {
+                for (int j = 0; j < alto; j++) {
+                    Color c = new Color(imgBin.getRGB(i, j));
+                    int r = (int) (c.getRed() * 0.299);
+                    int g = (int) (c.getGreen() * 0.587);
+                    int b = (int) (c.getBlue() * 0.114);
+                    int suma = r + g + b;
+                    if (suma > 128) {
+                        bin.setRGB(i, j, Color.WHITE.getRGB());
+                    } else {
+                        bin.setRGB(i, j, Color.BLACK.getRGB());
+                    }
+                }
+            }
+            ImageIcon prueba = new ImageIcon(bin);
+
+            contenedorImgProcesada.setIcon(prueba);
+
+            // Segmentación por Umbrales
+        }else if(e.paramString().indexOf("Umbral") != -1){
+            System.out.println("Umbral");
+
+            BufferedImage imgUmb = null;
+            try {
+                imgUmb = ImageIO.read(new File(PATH));
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+
+            BufferedImage umb = new BufferedImage(ancho, alto, BufferedImage.TYPE_BYTE_BINARY);
+
+            int umbral1 = 120; // Umbral 1
+            int umbral2 = 180; // Umbral 2
+            for (int i = 0; i < ancho; i++) {
+                for (int j = 0; j < alto; j++) {
+                    Color c = new Color(imgUmb.getRGB(i, j));
+                    int red = c.getRed();
+                    int green = c.getGreen();
+                    int blue = c.getBlue();
+                    int grayLevel = (red + green + blue) / 3;
+                    if (grayLevel < umbral1 || grayLevel > umbral2) {
+                        umb.setRGB(i, j, Color.WHITE.getRGB());
+                    } else {
+                        umb.setRGB(i, j, Color.BLACK.getRGB());
+                    }
+                }
+            }
+            ImageIcon prueba = new ImageIcon(umb);
+
+            contenedorImgProcesada.setIcon(prueba);
+        }else if(e.paramString().indexOf("Composicion") != -1){
+            System.out.println("Composición");
+
+            BufferedImage imgComp = null;
+            try {
+                imgComp = ImageIO.read(new File(PATH));
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+
+            Rectangle rect = contenedorImgProcesada.getBounds();
+            int x = rect.x + 58;
+            int y = rect.y + 104;
+            rect.setLocation(x, y);
+
+            Robot robot = null;
+            try {
+                robot = new Robot();
+
+            } catch (AWTException ex) {
+                throw new RuntimeException(ex);
+            }
+
+            System.out.println(rect);
+            BufferedImage imgCaptura = robot.createScreenCapture(rect);
+
+            BufferedImage imgProcesada = imgCaptura.getSubimage(0, 0, rect.width, rect.height);
+
+            BufferedImage imgResultado = new BufferedImage(ancho, alto, BufferedImage.TYPE_INT_ARGB);
+
+            // Dibujar la imagen1 en la imagen de destino
+            Graphics2D g2d = imgResultado.createGraphics();
+            g2d.drawImage(imgComp, 0, 0, null);
+
+            // Dibujar la imagen2 en la imagen de destino con una opacidad del 50%
+            g2d.setComposite(AlphaComposite.getInstance(
+                    AlphaComposite.SRC_OVER, 0.5f));
+            g2d.drawImage(imgProcesada, 0, 0, null);
+            g2d.dispose();
+
+            ImageIcon prueba = new ImageIcon(imgResultado);
+
+            contenedorImgProcesada.setIcon(prueba);
+        }else if(e.paramString().indexOf("Suma") != -1){
+            System.out.println("Suma");
+
+            BufferedImage imgSum = null;
+            try {
+                imgSum = ImageIO.read(new File(PATH));
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+
+            Rectangle rect = contenedorImgProcesada.getBounds();
+            int x = rect.x + 58;
+            int y = rect.y + 104;
+            rect.setLocation(x, y);
+
+            Robot robot = null;
+            try {
+                robot = new Robot();
+
+            } catch (AWTException ex) {
+                throw new RuntimeException(ex);
+            }
+
+            System.out.println(rect);
+            BufferedImage imgCaptura = robot.createScreenCapture(rect);
+
+            BufferedImage imgProcesada = imgCaptura.getSubimage(0, 0, ancho, alto);
+
+            BufferedImage imgResultado = new BufferedImage(ancho, alto, BufferedImage.TYPE_INT_ARGB);
+
+            for(int i = 0; i < ancho; i++){
+                for(int j = 0; j < alto; j++){
+                    int valorPixel1 = imgSum.getRGB(i, j);
+                    int valorPixel2 = imgProcesada.getRGB(i, j);
+                    int valorResultado = sumarValoresPixel(valorPixel1, valorPixel2);
+
+                    imgResultado.setRGB(i, j, valorResultado);
+                }
+            }
+            ImageIcon prueba = new ImageIcon(imgResultado);
+
+            contenedorImgProcesada.setIcon(prueba);
+
+
+        }else if(e.paramString().indexOf("Resta") != -1){
+            System.out.println("Resta");
+
+            BufferedImage imgRes = null;
+            try {
+                imgRes = ImageIO.read(new File(PATH));
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+
+            Rectangle rect = contenedorImgProcesada.getBounds();
+            int x = rect.x + 58;
+            int y = rect.y + 104;
+            rect.setLocation(x, y);
+
+            Robot robot = null;
+            try {
+                robot = new Robot();
+
+            } catch (AWTException ex) {
+                throw new RuntimeException(ex);
+            }
+
+            System.out.println(rect);
+            BufferedImage imgCaptura = robot.createScreenCapture(rect);
+
+            BufferedImage imgProcesada = imgCaptura.getSubimage(0, 0, ancho, alto);
+
+            BufferedImage imgResultado = new BufferedImage(ancho, alto, BufferedImage.TYPE_INT_ARGB);
+
+            for(int i = 0; i < ancho; i++){
+                for(int j = 0; j < alto; j++){
+                    int rgb1 = imgRes.getRGB(i, j);
+                    int rgb2 = imgProcesada.getRGB(i, j);
+                    int r1 = (rgb1 >> 16) & 0xFF;
+                    int g1 = (rgb1 >> 8) & 0xFF;
+                    int b1 = (rgb1 >> 0) & 0xFF;
+                    int r2 = (rgb2 >> 16) & 0xFF;
+                    int g2 = (rgb2 >> 8) & 0xFF;
+                    int b2 = (rgb2 >> 0) & 0xFF;
+                    int r = Math.abs(r1 - r2);
+                    int g = Math.abs(g1 - g2);
+                    int b = Math.abs(b1 - b2);
+                    int rgb = (r << 16) | (g << 8) | (b << 0);
+                    imgResultado.setRGB(i, j, rgb);
+                }
+            }
+            ImageIcon prueba = new ImageIcon(imgResultado);
+
+            contenedorImgProcesada.setIcon(prueba);
         }
+    }
+
+    public static int sumarValoresPixel(int valorPixel1, int valorPixel2) {
+        int alpha = (valorPixel1 >> 24) & 0xff;
+        int red = (valorPixel1 >> 16) & 0xff;
+        int green = (valorPixel1 >> 8) & 0xff;
+        int blue = valorPixel1 & 0xff;
+
+        int alpha2 = (valorPixel2 >> 24) & 0xff;
+        int red2 = (valorPixel2 >> 16) & 0xff;
+        int green2 = (valorPixel2 >> 8) & 0xff;
+        int blue2 = valorPixel2 & 0xff;
+
+        int alphaResultado = Math.min(255, alpha + alpha2);
+        int redResultado = Math.min(255, red + red2);
+        int greenResultado = Math.min(255, green + green2);
+        int blueResultado = Math.min(255, blue + blue2);
+
+        return (alphaResultado << 24) | (redResultado << 16) | (greenResultado << 8) | blueResultado;
     }
 }
