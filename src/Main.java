00350c6e4490be4be853b64e8c3dbae9adffbe1f
Uriel_Olaf <baezauriel01@gmail.com>
2023-03-23 10:22:02 -0600

    Ya la regamos

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
+<<<<<<< HEAD
     static String PATH = "C:\\Users\\Olaf\\Downloads\\khan.jpg";
+=======
+
+    //static String PATH = "C:\\Users\\daros\\OneDrive\\Escritorio\\Fotos\\bob.png";
+    static String PATH = "C:\\Users\\bmtz5\\OneDrive\\Imágenes\\imagen.jpg";
+>>>>>>> 1e788f19eb0d2ae2c7c58d2d38603593e7010526
     public static void main(String[] args) {
         System.out.println("Ejecutando proyecto");
 
